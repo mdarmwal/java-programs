ec694666ba10aa95fe5ffc5d0f4cde6f84c3b6ab
mdarmwal <127389722+mdarmwal@users.noreply.github.com>
2023-03-09 14:49:35 +0530

    multiplication

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+class Multiplication
+{
+    public static void main(String[] args)
+    {
+        int num1 = 20, num2 = 20, pro = 0;
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        pro = num1 / num2;
+        System.out.println("The product = " + pro);
+    }
+}
+    
