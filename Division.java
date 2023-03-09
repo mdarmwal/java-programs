3f136cb5377867115e5f5e96cad5f4badbb93a85
mdarmwal <127389722+mdarmwal@users.noreply.github.com>
2023-03-09 14:40:57 +0530

    division.java

---
+++ Division.java
@@ -0,0 +1,12 @@
+class Division
+{
+    public static void main(String[] args)
+    {
+        int num1 = 20, num2 = 20, quo = 0;
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        quo = num1 / num2;
+        System.out.println("The quotient = " + quo);
+    }
+}
+    
