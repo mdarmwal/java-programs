f476a896f852d26d08a5f4345d559d54765fe230
mdarmwal <127389722+mdarmwal@users.noreply.github.com>
2023-03-21 08:54:56 +0530

    Add files via upload

---
+++ Add2num.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Add2Num
+{
+public static void main(String args[])
+{
+System.out.print("\n Program to add two numbers using class -----");
+Num2 n1 = new Num2();
+n1.get_numbers();
+n1.add_print();
+}
+}
+class Num2
+{
+int num1, num2, sum;
+public void get_numbers()
+{
+Scanner reader = new Scanner(System.in);
+System.out.print("\nEnter the first number: ");
+num1 = reader.nextInt();
+System.out.print("\nEnter the second number: ");
+num2 = reader.nextInt();
+}
+public void add_print()
+{
+sum = num1 + num2;
+System.out.print("\nRequired sum: " + sum);
+}
+}
